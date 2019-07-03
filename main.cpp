83752c48593811d946d702e35f7465b2be5d3eec
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-07-04 05:43:36 +0900

    read

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include "librtmp/log.h"
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main( int argc, char **argv ) {
     if(argc<3){
-        RTMP_LogPrintf("need url and app and playpath: rtmp://127.0.0.1/app/playpath app playpath");
+        RTMP_LogPrintf("need IP APP PATH args. Ex: 127.0.0.1 app live");
         return 1;
     }
-    char *urlarg=argv[1];
+    char *iparg=argv[1];
     char *apparg=argv[2];
     char *playpatharg=argv[3];
     
@@ -19,7 +20,9 @@ int main( int argc, char **argv ) {
     AVal hostname={ argv[1], strlen(argv[1]) };
     AVal sockshost={ 0, 0 };
     AVal playpath={playpatharg,strlen(playpatharg)};
-    AVal tcUrl={ urlarg, strlen(urlarg) };
+    char url[1024];
+    snprintf(url,sizeof(url), "rtmp://%s/%s", iparg, apparg);
+    AVal tcUrl={ url, strlen(url) };
     AVal swfUrl={0,0};
     AVal pageUrl={0,0};
     AVal app={apparg,strlen(apparg)};
@@ -33,8 +36,24 @@ int main( int argc, char **argv ) {
                      &flashVer, &subscribepath, &usherToken, 0, 0, false, 30
                      );
 
-        //      RTMP_SetupStream(&rtmp, protocol, &hostname, port, &sockshost, &playpath,
-        //		       &tcUrl, &swfUrl, &pageUrl, &app, &auth, &swfHash, swfSize,
-        //		       &flashVer, &subscribepath, &usherToken, dSeek, dStopOffset, bLiveStream, timeout); 
-       
+
+    int r=RTMP_Connect(&rtmp,NULL);
+    fprintf(stderr, "RTMP_Connect return:%d\n", r);
+    if(!r) {
+        fprintf(stderr, "fatal, quit\n");
+        exit(1);
+    }
+    r = RTMP_ConnectStream(&rtmp,0);
+    fprintf(stderr, "RTMP_ConnectStream return:%d\n", r);
+
+    while(1) {
+        usleep(50*1000);
+        fprintf(stderr,".");
+        unsigned int nt=RTMP_GetTime();
+        size_t bufsize = 64*1024;
+        char *buffer = (char*)malloc(bufsize);
+        int nr=RTMP_Read(&rtmp,buffer,bufsize);
+        fprintf(stderr, "time:%u read:%d\n",nt,nr);
+    }
+    
 }
